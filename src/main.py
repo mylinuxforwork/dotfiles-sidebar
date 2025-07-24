eab87cdf084476ea5624b5c54845ad682609b57f
Stephan Raabe <mail@stephan-raabe.de>
2025-07-24 10:13:24 +0200

    Updates

--- src/main.py
+++ src/main.py
@@ -218,7 +218,7 @@ class DotfilesSidebarApplication(Adw.Application):
             application_name="ML4W Sidebar App",
             application_icon='com.ml4w.sidebar',
             developer_name="Stephan Raabe",
-            version="2.9.8.7",
+            version="2.9.8.8",
             website="https://github.com/mylinuxforwork/dotfiles-sidebar",
             issue_url="https://github.com/mylinuxforwork/dotfiles-sidebar/issues",
             support_url="https://github.com/mylinuxforwork/dotfiles-sidebar/issues",
